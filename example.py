b22ed8cdb9c914039ecd555951737fb494dd1df6
eightfold <97198162+8ightfold@users.noreply.github.com>
2023-09-26 19:34:09 -0600

    example with cleanup

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-from downloader import download_from_file, set_output_dir
+from downloader import download_from_file, cleanup_dirs
 from reuploader import upload_to_channel
 
 
 if __name__ == "__main__":
     download_from_file('videos.txt')
     upload_to_channel()
+    cleanup_dirs()
